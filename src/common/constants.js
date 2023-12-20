5a2a0697b85e819aa3ffe91a3f3b321ca2b99b5e
mchornomaz <chornomaz.maksym@gmail.com>
2023-12-20 19:58:49 +0200

    added data about portfolio projects

--- src/common/constants.js
+++ src/common/constants.js
@@ -137,8 +137,116 @@ const generalSkills = [
     },
 ]
 
+const layouts = [
+    {
+        id: 1,
+        title: 'Coffee Shop',
+        image: '/coffee-shop.png',
+        description: 'This website is developed for a store that sells coffee and related products from the free template found in web. The site is written in pure HTML, CSS, and JS, and it utilizes the ScrollReveal library to create beautiful animations when scrolling through the page. The website is fully responsive for mobile phones and tablets, and it looks great on any screen size. The site is created based on a template found on the internet.',
+        technologies: ['HTML5', 'CSS', 'JS', 'ScrollReveal'],
+        url: 'https://mchornomaz.github.io/coffe-shop/',
+        gitHubUrl: 'https://github.com/MChornomaz/coffe-shop'
+    },
+    {
+        id: 2,
+        title: 'Elixir',
+        image: '/elixir.png',
+        description: 'This website serves as a portfolio for a web design and development studio. The site is built using pure HTML, CSS, and JS, with styling based on the Bootstrap library. The Animate On Scroll library is employed for adding animations when scrolling. The FancyBox library is used to display images in fullscreen mode with slider functionality. The website is fully responsive for mobile phones and tablets, and it looks great on any screen size. The site is created based on a template found on the internet.',
+        technologies: ['HTML5', 'CSS', 'JS', 'Bootstrap', "Animate On Scroll", 'Fancybox'],
+        url: 'https://mchornomaz.github.io/elixir/',
+        gitHubUrl: 'https://github.com/MChornomaz/elixir'
+    },
+    {
+        id: 3,
+        title: 'BestBank',
+        image: '/bestbank.png',
+        description: 'This website serves as a landing page for a new banking application. The site is written in pure HTML, CSS, and JS, with primary styling done using the Tailwind CSS library and minimal use of raw CSS. No additional libraries were used other than Tailwind CSS. The site is created based on a template found on the internet. It is fully responsive for mobile phones and tablets, and it looks great on any screen size.',
+        technologies: ['HTML5', 'CSS', 'JS', 'Tailwind CSS'],
+        url: 'https://mchornomaz.github.io/best-bank/',
+        gitHubUrl: 'https://github.com/MChornomaz/best-bank'
+    },
+    {
+        id: 4,
+        title: 'Christmas gift',
+        image: '/xmas.png',
+        description: 'This website is a landing page for selling Christmas gifts. The site is written in pure HTML, CSS, and JS, and it utilizes the ScrollReveal library to create beautiful animations when scrolling through the page. The website is fully responsive for mobile phones and tablets, and it looks great on any screen size. It is created based on a template found on the internet.',
+        technologies: ['HTML5', 'CSS', 'JS', 'ScrollReveal'],
+        url: 'https://mchornomaz.github.io/xmas/',
+        gitHubUrl: 'https://github.com/MChornomaz/xmas'
+    },
+    {
+        id: 5,
+        title: 'Deep Slider',
+        image: '/deep-slider.png',
+        description: "This page is not a full-fledged website but rather demonstrates the functionality of a 3D slider that rotates along the z-axis, not horizontally or vertically, but towards or away from the user. The entire functionality is implemented using HTML5, JS, and SCSS. Gulp and Webpack were used to build the project, transforming SCSS into CSS, minimizing the entire code, compressing and reformatting images, and performing various other functions to enhance the page's performance and speed. The content of the page is not adapted for mobile devices, as it was primarily designed for wide screens where it maximally showcases itself.",
+        technologies: ['HTML5', 'SCSS', 'JS', 'Gulp', 'Webpack'],
+        url: 'https://mchornomaz.github.io/deep-slider/',
+        gitHubUrl: 'https://github.com/MChornomaz/deepScrollGallery'
+    },
+    {
+        id: 6,
+        title: 'Fairy Forest',
+        image: '/fairy-forest.png',
+        description: "This page is not a full-fledged website but showcases a beautiful parallax effect. The entire functionality is implemented using HTML5, JS, and SCSS. Gulp and Webpack were used to build the project, transforming SCSS into CSS, minimizing the entire code, compressing and reformatting images, and performing various other functions to enhance the page's performance and speed. The GSAP library was used for animation. The website is fully responsive for mobile phones and tablets and looks great on any screen size.",
+        technologies: ['HTML5', 'SCSS', 'JS', 'Gulp', 'Webpack', "GSAP"],
+        url: 'https://mchornomaz.github.io/fary-forest/',
+        gitHubUrl: 'https://github.com/MChornomaz/fary-forest-src'
+    },
+    {
+        id: 7,
+        title: 'Discover Beauty',
+        image: '/discover.png',
+        description: "This page serves as the first page of an informational website for travelers. The page features a minimalist design with minimal animations but incorporates two color schemes, light and dark. The entire functionality is implemented using HTML5, JS, and SCSS. Gulp and Webpack were used to build the project, transforming SCSS into CSS, minimizing the entire code, compressing and reformatting images, and performing various other functions to enhance the page's performance and speed. The website is fully responsive for mobile phones and tablets and looks great on any screen size. It is created based on a template found on the internet.",
+        technologies: ['HTML5', 'SCSS', 'JS', 'Gulp', 'Webpack'],
+        url: 'https://mchornomaz.github.io/DiscoverBeautyLayout/',
+        gitHubUrl: 'https://github.com/MChornomaz/DiscoverBsrc'
+    },
+    {
+        id: 8,
+        title: 'Building Company Website',
+        image: '/building.png',
+        description: "This page serves as the first page of a website for a construction company. The page features an engaging design with minimal animations. There are various variations of tabs and sliders. The entire functionality is implemented using HTML5, JS, and SCSS. Gulp and Webpack were used to build the project, transforming SCSS into CSS, minimizing the entire code, compressing and reformatting images, and performing various other functions to enhance the page's performance and speed. The website is fully responsive for mobile phones and tablets and looks great on any screen size. It is created based on a template found on the internet.",
+        technologies: ['HTML5', 'SCSS', 'JS', 'Gulp', 'Webpack'],
+        url: 'https://mchornomaz.github.io/BuildingCompanyLayout/',
+        gitHubUrl: 'https://github.com/MChornomaz/BuildingCompSrc'
+    }
+]
+
+const reactStack = [
+    {
+        id: 1,
+        title: 'Spotify',
+        image: '/spotify.png',
+        description: 'This project is an original clone of Spotify with very similar functionality. It allows users to choose and listen to songs of various genres, display top songs by genres and charts, search for artists, show similar songs, and display song lyrics. The client-side was developed using React and Redux Toolkit, with styling done using Tailwind CSS. The Swiper library was utilized for creating sliders. The Shazam core API was used for the server-side implementation.',
+        technologies: ['React', 'Redux Toolkit', 'Tailwind CSS', 'Axios', 'React Router Dom', 'Swiper'],
+        url: '',
+        gitHubUrl: 'https://github.com/MChornomaz/spotify-clone'
+    },
+    {
+        id: 2,
+        title: 'Crypto App',
+        image: '/crypto.png',
+        description: 'This project is dedicated to cryptocurrency tracking. It gathers information about the most popular cryptocurrencies and utilizes Chart JS to display their price changes in the form of diagrams. The project is built with React, and the state management is handled using Redux Toolkit. Styling is done with the Ant Design library. The server-side is powered by RapidAPI, fetching cryptocurrency information from CoinRanking API and cryptocurrency news from Bing News Search API.',
+        technologies: ['React', 'Redux Toolkit', 'Ant Design', 'Axios', 'Chart JS', 'Millify'],
+        url: "",
+        gitHubUrl: 'https://github.com/MChornomaz/crypto-app'
+    },
+]
+
+const fullStack = [
+    {
+        id: 1,
+        title: 'BSA Talents',
+        image: '/bsa.png',
+        description: "This project was developed as part of a team of 8 developers. The essence of the application is to facilitate connections between students and graduates of an IT academy and employers. The functionality is similar to that of Djinny. It is a full-stack project based on React on the client side and Fastify + PostgreSQL on the server side, utilizing SocketIO for chat functionality. Files were hosted on AWS. Additionally, there is integration with the academy's LMS API. The project is more educational, and deployment is currently not available.",
+        technologies: ['TypeScript', 'React', 'React Router Dom', 'Redux Toolkit', 'SocketIO', 'Material UI', 'SCSS', 'Fastify', 'Swagger', 'AWS SDK', 'BCrypt', 'Knex', 'PostgreSQL' ],
+        url: '',
+        gitHubUrl: 'https://github.com/BinaryStudioAcademy/bsa-2023-bsa-talents'
+    },
+]
 
 
 
 
-export {frontendSkills, backendSkills, generalSkills}
+
+export {frontendSkills, backendSkills, generalSkills, layouts, reactStack, fullStack}
